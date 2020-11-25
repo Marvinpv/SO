7837848e517b67f8688c7872cc4c834c1b21b59b
MarvinPV <marvin.peinado@gmail.com>
2020-11-25 17:02:54 +0100

    Ejercicio 4 compilado

--- ejercicio4.c
+++ ejercicio4.c
@@ -7,22 +7,28 @@
 #include<stdlib.h>
 #include<dirent.h>
 #include<string.h>
+#include<sys/wait.h>
 
 int main(int argc, char *argv[]){
     pid_t pid;
     pid_t hijo_esperado;
+    int i;
 
     for(i = 0 ; i < 5 ; i++){
         if((pid = fork()) < 0){
             perror("Error en el fork\n");
             exit(-1);
-        }else if(!pid){
-            continue;
+        }
+        
+        if(!pid){
+            break;
         }
     }
+
+
     if(pid){
         while(i > 0){
-            hijo_esperado = wait(WEXITED);
+            hijo_esperado = wait(0);
             printf("Ha terminado mi hijo con PID: %d\n",hijo_esperado);
             i--;
             printf("Quedan %d hijos executando...\n",i);
